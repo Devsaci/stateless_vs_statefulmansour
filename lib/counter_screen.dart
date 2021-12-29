b327ff2f1a352ce04abf152f50627a6eb3d5794c
saci zakaria <sacideveloppeur@gmail.com>
2021-12-29 15:41:59 +0100

    Center / Padding

--- lib/counter_screen.dart
+++ lib/counter_screen.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CounterScreen extends StatelessWidget {
   const CounterScreen({Key? key}) : super(key: key);
@@ -12,7 +16,31 @@ class CounterScreen extends StatelessWidget {
         title: Text("StateLess VS StateFul"),
         centerTitle: true,
       ),
-      body: Center(child: Text("StateLess VS StateFul"),),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text("Minus"),
+              ),
+              Text(
+                "7",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("Plus"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
